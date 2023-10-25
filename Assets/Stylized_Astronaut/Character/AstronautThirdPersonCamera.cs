31eccf900a336c337b38e271157574b730b14874
Mumin Ali <muminali13@hotmail.co.uk>
2023-10-25 14:47:30 +0100

    fixed terrain, camera movement

--- Assets/Stylized_Astronaut/Character/AstronautThirdPersonCamera.cs
+++ Assets/Stylized_Astronaut/Character/AstronautThirdPersonCamera.cs
@@ -8,7 +8,7 @@ namespace AstronautThirdPersonCamera
   public class AstronautThirdPersonCamera : MonoBehaviour
   {
     private const float Y_ANGLE_MIN = 0.0f;
-    private const float Y_ANGLE_MAX = 50.0f;
+    private const float Y_ANGLE_MAX = 80.0f;
 
     public Transform lookAt;
     public Transform camTransform;
@@ -16,18 +16,21 @@ namespace AstronautThirdPersonCamera
 
     private float currentX = 0.0f;
     private float currentY = 45.0f;
+    public float mouseSensitivity = 2.5f;
     private float sensitivityX = 20.0f;
     private float sensitivityY = 20.0f;
 
     private void Start()
     {
         camTransform = transform;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
-        currentX += Input.GetAxis("Mouse X");
-        currentY += Input.GetAxis("Mouse Y");
+        currentX += Input.GetAxis("Mouse X") * mouseSensitivity;
+        currentY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
     }
